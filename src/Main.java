d496b1f4e4c660747f29bebbbaff524ad1a4c486
ManasesIstvan <figoistvan@yahoo.com>
2022-01-27 16:40:03 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        short myShort = 2;
+
+        short mySeccondShort;
+
+        mySeccondShort = 2;
+
+        int myInt = mySeccondShort + 2;
+
+        System.out.println(myInt);
+
+        long mySecondInt = 123562132134L;
+        long result = myInt + 12362131232L;
+        float myFloat = 3.3F;
+        double myDouble = myFloat + myInt;
+
+
+        System.out.println(2 + 2 + 21 - 2 * (2 + 2));
+        System.out.println("SzeriiBebeee<3");
+        System.out.println("MyInt este: " + myInt);
+
+        String myString = "Hello World 2";
+        System.out.println(myString + myInt + 2);
+
+        byte age = 21;
+        String gameName = "TombRaider";
+        String appName = "YouTube";
+
+        String story = "Bubi are " + age + " de ani, se joaca " + gameName + " si nu stie cum sa avanseze un nivel " +
+                "dar se uita pe " + appName + " si afta cum sa treaca";
+
+        System.out.println(story);
+
+    }
+
+
+}
